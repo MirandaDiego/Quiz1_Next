0f4f883a136306213eee3e428764ada89b50d14d
MirandaDiego <diegoflam12@gmail.com>
2023-08-02 19:10:43 -0300

    quiz1

---
+++ src/app/data/questions.ts
@@ -0,0 +1,14 @@
+export const questions = [
+    {
+        question: 'Qual o Diego prefere?',
+        options: [
+            'Loiras',
+            'Morenas',
+            'Ruivas',
+            'Negras',
+            'Traveco',
+            'Todas'
+        ],
+        answer: 6
+    }
+]
